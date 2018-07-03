f744aff52d7cd4d592a89df0168c6152f13ff1b9
Iván Iguarán <ivan.iguaran@ideaware.co>
2018-07-03 12:27:02 -0500

    Fix before install script

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,4 +2,4 @@
 
 cd /home/ubuntu/api-release
 echo "$(date '+%F %T') Stopping server" >> /home/ubuntu/deployment_logs/stop_server.log 2>&1
-lsof -ti:1337 | xargs kill >> /home/ubuntu/deployment_logs/stop_server.log 2>&1
+lsof -ti:1337 | xargs kill >> /home/ubuntu/deployment_logs/stop_server.log > /dev/null 2> /dev/null < /dev/null

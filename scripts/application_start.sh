c9fe9e98ce11f1d54f76e2c531794dd2023cbcc1
Iván Iguarán <ivan.iguaran@ideaware.co>
2018-07-03 13:18:52 -0500

    End correctly application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -7,4 +7,4 @@ sudo rsync -arv --delete \
   >> /home/ubuntu/deployment_logs/rsync.log 2>&1
 
 echo "$(date '+%F %T') Starting server" >> /home/ubuntu/deployment_logs/start_server.log 2>&1
-cd /home/ubuntu/cca-lbm-cms-api && npm start --development >> /home/ubuntu/deployment_logs/start_server.log 2>&1
+cd /home/ubuntu/cca-lbm-cms-api && npm start --development >> /home/ubuntu/deployment_logs/start_server.log > /dev/null 2> /dev/null < /dev/null &

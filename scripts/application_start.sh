558925654638b1fdccba0f982e18b09ba6e94758
Iván Iguarán <ivan.iguaran@ideaware.co>
2018-07-12 09:12:53 -0500

    Run as sudo

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -5,7 +5,7 @@
 #
 
 echo "$(date '+%F %T') rsyncing release folder with api folder" >> /home/ubuntu/deployment_logs/rsync.log 2>&1
-sudo rsync -arv --delete \
+rsync -arv --delete \
   /home/ubuntu/api-release/ \
   /home/ubuntu/cca-lbm-cms-api/ \
   >> /home/ubuntu/deployment_logs/rsync.log 2>&1
@@ -21,11 +21,11 @@ SecurityGroup=`/usr/bin/curl -s http://169.254.169.254/latest/meta-data/security
 if [ "$SecurityGroup" = "cca-lbm-cms-api-production" ]; then
   cd /home/ubuntu/cca-lbm-cms-api
   echo "$(date '+%F %T') Starting server" >> /home/ubuntu/deployment_logs/start_server.log 2>&1
-  sudo npm start --production >> /home/ubuntu/deployment_logs/start_server.log > /dev/null 2> /dev/null < /dev/null &
+  npm start --production >> /home/ubuntu/deployment_logs/start_server.log > /dev/null 2> /dev/null < /dev/null &
 fi
 
 if [ "$SecurityGroup" = "cca-lbm-cms-api-develop" ]; then
   cd /home/ubuntu/cca-lbm-cms-api
   echo "$(date '+%F %T') Starting server" >> /home/ubuntu/deployment_logs/start_server.log 2>&1
-  sudo npm start --development >> /home/ubuntu/deployment_logs/start_server.log > /dev/null 2> /dev/null < /dev/null &
+  npm start --development >> /home/ubuntu/deployment_logs/start_server.log > /dev/null 2> /dev/null < /dev/null &
 fi

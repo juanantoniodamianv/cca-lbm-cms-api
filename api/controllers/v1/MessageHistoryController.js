cb91ce9a8abd4012840df1f6620dd0dcffe77c39
Juan Antonio Damian Vargas <juanantoniodamianv@gmail.com>
2018-10-01 17:14:34 -0300

    Update filename on export CSV.

--- api/controllers/v1/MessageHistoryController.js
+++ api/controllers/v1/MessageHistoryController.js
@@ -29,7 +29,7 @@ module.exports = {
         }
       };
       
-      res.setHeader('Content-disposition', 'attachment; filename=report.csv');
+      res.setHeader('Content-disposition', 'attachment; filename="report.csv"');
       res.set('Content-Type', 'text/csv');
       
       
